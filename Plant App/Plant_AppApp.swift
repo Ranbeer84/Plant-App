73aa419f6fd7b17a0f973c4aa2ea1baa860992ee
Ranbeer Singh <Ranbeersinghcauhan@gmail.com>
2022-10-16 08:29:04 +0530

    Initial Commit

---
+++ Plant App/Plant_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Plant_AppApp.swift
+//  Plant App
+//
+//  Created by RANBEER SINGH on 16/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Plant_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
